e79152de43dad9dc696ad2c0c326162810d508e9
Ezeko <ezeko2017@gmail.com>
2020-07-01 22:40:37 +0100

    bug
    change expenditure to use noemal database

--- app/Expenditure.php
+++ app/Expenditure.php
@@ -8,5 +8,5 @@ class Expenditure extends Model
 {
     //the corresponding table returns data from the EXPENDITURE xcel file
 
-    protected $table = "EconomicExpenditure"; //this is the name of the table in the DB
+    protected $table = "expenses"; //this is the name of the table in the DB
 }
