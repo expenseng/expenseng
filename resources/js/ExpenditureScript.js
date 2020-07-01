d594c8bbec7584669afed9476e8f2116e8b1e90c
Ezeko <ezeko2017@gmail.com>
2020-07-02 00:54:56 +0100

    bug
    add semi-colon to js

--- resources/js/ExpenditureScript.js
+++ resources/js/ExpenditureScript.js
@@ -33,7 +33,7 @@ function apply(){
 
 
 let showPopup = function(){
-    document.querySelector('.section-wrapper').classList.add('show')
+    document.querySelector('.section-wrapper').classList.add('show');
 }
 let profileSummary = document.querySelectorAll(".profileSummary").forEach
 (profileSummary => profileSummary.addEventListener('click' , showPopup));
@@ -41,6 +41,6 @@ let profileSummary = document.querySelectorAll(".profileSummary").forEach
 let closeButton = document.querySelector('.section-wrapper .close-button');
 
 let hidePopup = function(){
-    document.querySelector('.section-wrapper').classList.remove('show')
+    document.querySelector('.section-wrapper').classList.remove('show');
 }
 closeButton.addEventListener('click', hidePopup);
