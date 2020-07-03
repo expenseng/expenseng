84da1b53775beb78a06f6a8fe9c2c7c8dff3565b
papa28x4 <swissfair@yahoo.co.uk>
2020-07-03 17:28:35 +0100

    code formatting 3

--- app/Http/Controllers/Expenditure.php
+++ app/Http/Controllers/Expenditure.php
@@ -19,11 +19,11 @@ class Expenditure extends Controller
     public function store()
     {
         DB::insert(
-        'insert into monthly_budget_economicexpenditure 
-        (name, budget, allocation, total_allocation, balance) values(?,?,?,?,?)',
-        ['2nd Niger Bridge', '10000000000', '1000000000', '2000000000', '7000000000']);
-        $users = DB::select('select * from monthly_budget_economicexpenditure'
+            'insert into monthly_budget_economicexpenditure 
+            (name, budget, allocation, total_allocation, balance) values(?,?,?,?,?)',
+            ['2nd Niger Bridge', '10000000000', '1000000000', '2000000000', '7000000000']
         );
+        $users = DB::select('select * from monthly_budget_economicexpenditure');
         return $users;
     }
 }
