6c600a2401f3589fa2ebb730ff4507ef41b99077
Ezeko <ezeko2017@gmail.com>
2020-07-06 09:56:23 +0100

    style
    style filters to match with figma design

--- resources/views/pages/ministry_profile.blade.php
+++ resources/views/pages/ministry_profile.blade.php
@@ -97,7 +97,7 @@
                 <div class="col-md-6"><h6>Graph(Daily): 12 May, 2019</h6></div>
 				<div class="col-md-6 table-top">
 					<h5>20th May, 2020</h5>
-					<button><h5>Select Date <i class="fas fa-filter"></i></button></h5>
+					<button id="filter"><h5 >Select Date <i class="fas fa-filter"></i></button></h5>
                 </div>
                 <div class="text-left col-md-2">
                     <img src="/images/graph.png"  class="responsive" alt="graph">
@@ -109,7 +109,7 @@
                 <div class="col-md-6"><h6>Graph(Monthly): May, 2019</h6></div>
 				<div class="col-md-6 table-top">
 					<h5> May, 2020</h5>
-					<button><h5>Select Month <i class="fas fa-filter"></i></button></h5>
+					<button id="filter"><h5 >Select Month <i class="fas fa-filter"></i></button></h5>
                 </div>
                 <div class="text-left col-md-2">
                     <img src="/images/graph.png"  class="responsive" alt="graph">
@@ -121,7 +121,7 @@
                 <div class="col-md-6"><h6>Graph(Yearly):2019</h6></div>
 				<div class="col-md-6 table-top">
 					<h5>2020</h5>
-					<button><h5>Select Year <i class="fas fa-filter"></i></button></h5>
+					<button id="filter"><h5>Select Year <i class="fas fa-filter"></i></button></h5>
                 </div>
                 <div class="text-left col-md-2">
                     <img src="/images/graph.png"  class="responsive" alt="graph">
