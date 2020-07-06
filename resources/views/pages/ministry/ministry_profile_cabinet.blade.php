1b02a73dc075a98bab921bb9695fca10ef4660e5
Ezeko <ezeko2017@gmail.com>
2020-07-06 11:57:21 +0100

    refactor
    create charts, and cabinet

---
+++ resources/views/pages/ministry/ministry_profile_cabinet.blade.php
@@ -0,0 +1,75 @@
+<div class="row">
+    <div class="card col-md-4" style="width: 18rem;">
+    <img class="card-img-top" src="/images/img1.png" alt="ministers Image">
+    <div class="card-body">
+        <h5 class="card-title">Engr Jafaru Damulak</h5>
+        <p class="card-text">Minister of Works</p>
+        <!--social media-->
+        <div class="row">
+            <div class="col-md-3">
+                <a href="#" class=""><i class="fa fa-twitter"></i></a>
+            </div>
+            <div class="col-md-3">
+                <a href="#" class=""><i class="fa fa-facebook"></i></a>
+            </div>
+            <div class="col-md-3">
+                <a href="#" class=""><i class="fa fa-linkedin"></i></a>
+            </div>
+            <div class="col-md-3">
+                <a href="#" class=""><i class="fa fa-goggle"></i></a>
+            </div>
+        </div>
+    </div>
+    </div>
+    <!--end of card -->
+
+    <div class="card col-md-4" style="width: 18rem;">
+    <img class="card-img-top" src="/images/img2.png" alt="Minister of state">
+    <div class="card-body">
+        <h5 class="card-title">Alhaji Zubairu I. Bayi</h5>
+        <p class="card-text">Minister of State Works</p>
+
+        <!--social media-->
+        <div class="row">
+            <div class="col-md-3">
+                <a href="#" class=""><i class="fa fa-twitter"></i></a>
+            </div>
+            <div class="col-md-3">
+                <a href="#" class=""><i class="fa fa-facebook"></i></a>
+            </div>
+            <div class="col-md-3">
+                <a href="#" class=""><i class="fa fa-linkedin"></i></a>
+            </div>
+            <div class="col-md-3">
+                <a href="#" class=""><i class="fa fa-goggle"></i></a>
+            </div>
+        </div>
+        
+    </div>
+    </div>
+    <!--end of card -->
+
+    <div class="card col-md-4" style="width: 18rem;">
+    <img class="card-img-top" src="/images/img3.png" alt="Director">
+    <div class="card-body">
+        <h5 class="card-title">Engr. Goni M. Shiekh</h5>
+        <p class="card-text">Director Fedral Department of works</p>
+        <!--social media-->
+        <div class="row">
+            <div class="col-md-3">
+                <a href="#" class=""><i class="fa fa-twitter"></i></a>
+            </div>
+            <div class="col-md-3">
+                <a href="#" class=""><i class="fa fa-facebook"></i></a>
+            </div>
+            <div class="col-md-3">
+                <a href="#" class=""><i class="fa fa-linkedin"></i></a>
+            </div>
+            <div class="col-md-3">
+                <a href="#" class=""><i class="fa fa-goggle"></i></a>
+            </div>
+        </div>
+    </div>
+    </div>
+    <!--end of card-->
+</div>
--- resources/views/pages/ministry_profile.blade.php
+++ resources/views/pages/ministry_profile.blade.php
@@ -92,44 +92,8 @@
 <div class="mb-5 mt-3 row">
     <div class="col-md-8">
         <h5 id="charts">Charts</h5>
-    <div class="table-section reponsive-div">
-			<div class="row main-table">
-                <div class="col-md-6"><h6>Graph(Daily): 12 May, 2019</h6></div>
-				<div class="col-md-6 table-top">
-					<h5>20th May, 2020</h5>
-					<button id="filter"><h5 >Select Date <i class="fas fa-filter"></i></button></h5>
-                </div>
-                <div class="text-left col-md-2">
-                    <img src="/images/graph.png"  class="responsive" alt="graph">
-                </div>
-            </div>
-    </div>
-    <div class="table-section reponsive-div">
-			<div class="row main-table">
-                <div class="col-md-6"><h6>Graph(Monthly): May, 2019</h6></div>
-				<div class="col-md-6 table-top">
-					<h5> May, 2020</h5>
-					<button id="filter"><h5 >Select Month <i class="fas fa-filter"></i></button></h5>
-                </div>
-                <div class="text-left col-md-2">
-                    <img src="/images/graph.png"  class="responsive" alt="graph">
-                </div>
-            </div>
-    </div>
-    <div class="table-section reponsive-div">
-			<div class="row main-table">
-                <div class="col-md-6"><h6>Graph(Yearly):2019</h6></div>
-				<div class="col-md-6 table-top">
-					<h5>2020</h5>
-					<button id="filter"><h5>Select Year <i class="fas fa-filter"></i></button></h5>
-                </div>
-                <div class="text-left col-md-2">
-                    <img src="/images/graph.png"  class="responsive" alt="graph">
-                </div>
-            </div>
-    </div>
-
-
+        @include('pages.ministry.ministry_profile_cabinet')
+    
 
   </div>
     <!--end of chart-->
