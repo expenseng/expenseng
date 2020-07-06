69f804c60efe5cd4cda8e86a4c1f144d99e1c511
Ezeko <ezeko2017@gmail.com>
2020-07-06 17:08:22 +0100

    style
    style headers

--- resources/views/pages/ministry_profile.blade.php
+++ resources/views/pages/ministry_profile.blade.php
@@ -17,12 +17,11 @@
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" id="home" href="#" style="color: #00945E;">Home <span class="sr-only">(current)</span>
+        <a class="nav-link" id="home" href="#" style="color: #00945E;">
+        Home <span class="sr-only">(current)</span>
         </a>
       </li>
-      <li class="nav-item" id="dot"></li>
-      
-      
+
       <li class="nav-item">
         <a class="nav-link" id="profile" href="#">Profile</a>
       </li>
@@ -41,18 +40,18 @@
 <section id="main" class="container">
 <div class="row p-2 ">
 
-    <div class="text-left col-md-2">
+    <div class="text-left col-md-1">
     <img src="/images/nglogo2.png" id="ministry_logo" class="rounded" alt="fg logo">
     </div>
 
-    <div id="ministry_name" class="p-2 col-md-4">
+    <div id="ministry_name" class=" col-md-7">
         <p >Ministry of Works And Human Development</p>
     </div>
 
 </div>
 
 <div class="mt-5 row">
-  <div class="p-2 col-md-4">
+  <div class="p-1 col-md-4">
 
       <div class="d-flex flex-column">
         <div class="p-2">Ministry Twitter Handle</div>
@@ -61,7 +60,7 @@
       </div>
 
   </div>
-  <div class="p-2 col-md-4">
+  <div class="p-1 col-md-4">
      <div class="d-flex flex-column ">
         <div class="p-2">Total Amount Spent</div>
         <div class="p-2" id="color">#38.6M</div>
@@ -69,7 +68,7 @@
       </div>
   </div>
 
-  <div class="p-2 col-md-4">
+  <div class="p-1 col-md-4">
      <div class="d-flex flex-column">
         <div class="p-2">Total Number of Projects Contracted</div>
         <div class="p-2" id="color">27</div>
