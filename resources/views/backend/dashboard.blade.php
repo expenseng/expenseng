674fe882573311870e705996608809bc827319f6
Ezeko <ezeko2017@gmail.com>
2020-07-07 20:40:19 +0100

    bug
    resolve image to exact path

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -127,7 +127,7 @@
                                                     <tr>
                                                         <td>1</td>
                                                         <td>
-                                                            <div class="m-r-10"><img src="assets/images/product-pic.jpg" alt="user" class="rounded" width="45"></div>
+                                                            <div class="m-r-10"><img src="/images/product-pic.jpg" alt="user" class="rounded" width="45"></div>
                                                         </td>
                                                         <td>Product #1 </td>
                                                         <td>id000001 </td>
@@ -140,7 +140,7 @@
                                                     <tr>
                                                         <td>2</td>
                                                         <td>
-                                                            <div class="m-r-10"><img src="assets/images/product-pic-2.jpg" alt="user" class="rounded" width="45"></div>
+                                                            <div class="m-r-10"><img src="/images/product-pic-2.jpg" alt="user" class="rounded" width="45"></div>
                                                         </td>
                                                         <td>Product #2 </td>
                                                         <td>id000002 </td>
@@ -153,7 +153,7 @@
                                                     <tr>
                                                         <td>3</td>
                                                         <td>
-                                                            <div class="m-r-10"><img src="assets/images/product-pic-3.jpg" alt="user" class="rounded" width="45"></div>
+                                                            <div class="m-r-10"><img src="/images/product-pic-3.jpg" alt="user" class="rounded" width="45"></div>
                                                         </td>
                                                         <td>Product #3 </td>
                                                         <td>id000003 </td>
@@ -166,7 +166,7 @@
                                                     <tr>
                                                         <td>4</td>
                                                         <td>
-                                                            <div class="m-r-10"><img src="assets/images/product-pic-4.jpg" alt="user" class="rounded" width="45"></div>
+                                                            <div class="m-r-10"><img src="/images/product-pic-4.jpg" alt="user" class="rounded" width="45"></div>
                                                         </td>
                                                         <td>Product #4 </td>
                                                         <td>id000004 </td>
