c1c34faf48ccd493a4236c3eca93cbe6086c0888
Ezeko <ezeko2017@gmail.com>
2020-07-07 19:41:39 +0100

    fix
    fix code styling errors

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -6,11 +6,13 @@ use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
-    public function expense(){
+    public function expense()
+    {
         return view('pages.expense.report');
     }
 
-    public function ministry(){
+    public function ministry()
+    {
         return view('pages.ministry.ministry_report_comments');
     }
 }
