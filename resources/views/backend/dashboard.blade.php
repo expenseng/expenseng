cab272cd67b4bd24f94eb4bf00a02dcc30bfafac
Ezeko <ezeko2017@gmail.com>
2020-07-07 21:29:22 +0100

    change vendor

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -21,7 +21,7 @@
                     <div class="row">
                         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                             <div class="page-header">
-                                <h2 class="pageheader-title">ExpenseNg Admin Dashboard Template </h2>
+                                <h2 class="pageheader-title">ExpenseNg Admin Dashboard </h2>
                                 <p class="pageheader-text">Nulla euismod urna eros, sit amet scelerisque torton lectus vel mauris facilisis faucibus at enim quis massa lobortis rutrum.</p>
                                 <div class="page-breadcrumb">
                                     <nav aria-label="breadcrumb">
