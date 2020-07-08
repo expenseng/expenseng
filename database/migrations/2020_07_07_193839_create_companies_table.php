678284e4d162c51d33b664a4c100a88a6c385b5f
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-07-08 17:53:24 +0100

    fix:updated db migrations

--- database/migrations/2020_07_07_193839_create_companies_table.php
+++ database/migrations/2020_07_07_193839_create_companies_table.php
@@ -15,7 +15,7 @@ class CreateCompaniesTable extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->unique();
+            $table->string('name', 191)->unique();
             $table->string('industry');
             $table->string('ceo');
             $table->string('twitter');
