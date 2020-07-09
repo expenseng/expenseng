dc96105bfea120102db5e48ed7c5a5b67b471f06
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-07-09 04:29:59 +0100

    bug:fix budgetseeder

--- app/Budget.php
+++ app/Budget.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Budget extends Model
 {
-    //
+    protected $fillable = [
+        "amount", "project_name", 
+        "code", "year", "classification"
+    ];
 }
--- database/seeds/BudgetSeeder.php
+++ database/seeds/BudgetSeeder.php
@@ -2,168 +2,164 @@
 
 use App\Budget;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
 class BudgetSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the database seeds
      *
      * @return void
      */
     public function run()
     {
-        Budget::create(
+        DB::table('budgets')->insert([
             [
-                'amount' => "5236425478125.26",
+                'amount' => "523642547812526",
                 'project_name' => "General Public Service",
                 'code' => "1-GPS",
                 'year' => "2016",
                 'classification' => "Functions of Government",
             ],
             [
-                'amount' => "376566119970.03",
+                'amount' => "37656611997003",
                 'project_name' => "General Public Service",
                 'code' => "1-GPS",
                 'year' => "2017",
                 'classification' => "Functions of Government",
             ],
             [
-                'amount' => "236425478125.60",
+                'amount' => "23642547812560",
                 'project_name' => "General Public Service",
                 'code' => "1-GPS",
                 'year' => "2018",
                 'classification' => "Functions of Government",
             ],
             [
-                'amount' => "326,425,478,125.26",
+                'amount' => "32642547812526",
                 'project_name' => "General Public Service",
                 'code' => "1-GPS",
                 'year' => "2019",
                 'classification' => "Functions of Government",
             ],
             [
-                'amount' => "5,236,425,478,125.26",
+                'amount' => "523642547812526",
                 'project_name' => "General Public Service",
                 'code' => "1-GPS",
                 'year' => "2020",
                 'classification' => "Functions of Government",
             ],
-            // Defence
             [
-                'amount' => "376566119970.03",
+                'amount' => "37656611997003",
                 'project_name' => "Defence",
                 'code' => "2-Def",
                 'year' => "2016",
                 'classification' => "Functions of Government",
             ],
             [
-                'amount' => "236425478125.60",
+                'amount' => "23642547812560",
                 'project_name' => "Defence",
                 'code' => "2-Def",
                 'year' => "2017",
                 'classification' => "Functions of Government",
             ],
             [
-                'amount' => "326,425,478,125.26",
+                'amount' => "32642547812526",
                 'project_name' => "Defence",
                 'code' => "2-Def",
                 'year' => "2018",
                 'classification' => "Functions of Government",
             ],
             [
-                'amount' => "755324373982.63",
+                'amount' => "75532437398263",
                 'project_name' => "Defence",
                 'code' => "2-Def",
                 'year' => "2020",
                 'classification' => "Functions of Government",
             ],
-            // Education
             [
-                'amount' => "1009660430935.43",
+                'amount' => "100966043093543",
                 'project_name' => "Education",
                 'code' => "3-E",
                 'year' => "2016",
                 'classification' => "Functions of Government",
             ],
             [
-                'amount' => "236425478125.60",
+                'amount' => "23642547812560",
                 'project_name' => "Education",
                 'code' => "3-E",
                 'year' => "2017",
                 'classification' => "Functions of Government",
             ],
             [
-                'amount' => "326,425,478,125.26",
+                'amount' => "32642547812526",
                 'project_name' => "Education",
                 'code' => "3-E",
                 'year' => "2018",
                 'classification' => "Functions of Government",
             ],
             [
-                'amount' => "755324373982.63",
+                'amount' => "75532437398263",
                 'project_name' => "Education",
                 'code' => "3-E",
                 'year' => "2019",
                 'classification' => "Functions of Government",
             ],
-            // Health
             [
-                'amount' => "371200595611.349",
+                'amount' => "371200595611349",
                 'project_name' => "Health",
                 'code' => "7-H",
                 'year' => "2016",
                 'classification' => "Functions of Government",
             ],
             [
+                "amount" => "2312920831232",
                 'project_name' => "Health",
                 'code' => "7-H",
                 'year' => "2017",
                 'classification' => "Functions of Government",
             ],
             [
-                'amount' => "326,425,478,125.26",
+                'amount' => "32642547812526",
                 'project_name' => "Health",
                 'code' => "7-H",
                 'year' => "2018",
                 'classification' => "Functions of Government",
             ],
             [
-                'amount' => "755324373982.63",
+                'amount' => "75532437398263",
                 'project_name' => "Health",
                 'code' => "7-H",
                 'year' => "2016",
                 'classification' => "Functions of Government",
             ],
-            //Housing and Community Amenities
             [
-                'amount' => "371200595611.349",
+                'amount' => "371200595611349",
                 'project_name' => "Housing and Community Amenities",
                 'code' => "HCA",
                 'year' => "2016",
                 'classification' => "Functions of Government",
             ],
             [
-                'amount' => "23903492321.423",
+                'amount' => "23903492321423",
                 'project_name' => "Housing and Community Amenities",
                 'code' => "HCA",
                 'year' => "2017",
                 'classification' => "Functions of Government",
             ],
             [
-                'amount' => "326,425,478,125.26",
+                'amount' => "32642547812526",
                 'project_name' => "Housing and Community Amenities",
                 'code' => "HCA",
                 'year' => "2018",
                 'classification' => "Functions of Government",
             ],
             [
-                'amount' => "755324373982.63",
+                'amount' => "75532437398263",
                 'project_name' => "Housing and Community Amenities",
                 'code' => "HCA",
                 'year' => "2019",
                 'classification' => "Functions of Government",
             ],
-        );
-
+        ]);
     }
 }
