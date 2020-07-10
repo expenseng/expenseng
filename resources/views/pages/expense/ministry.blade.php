78806c3722673f098910e2c7824efe44ba43d16a
Nwokochahannah <hannahsopuruchi@gmail.com>
2020-07-10 09:26:04 +0100

    expense page

--- resources/views/pages/expense/ministry.blade.php
+++ resources/views/pages/expense/ministry.blade.php
@@ -336,4 +336,8 @@
 @section('js')
 	<script src="{{ asset('js/main.js') }}"></script>
 	<script src="{{ asset('js/ExpenditureScript.js') }}"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+	<script src="{{ asset('js/index.js') }}"></script>
 @endsection
