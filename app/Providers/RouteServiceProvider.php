d83444f2311cef0e098fcf6bdc6234586a858f00
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-07-12 21:01:41 +0100

    feat:(api) mapping api.php to api.dev.expenseng.com

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -72,7 +72,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::prefix('api')
+        Route::domain('api.dev.expenseng.com')
+        // Route::prefix('api')
             ->middleware('api')
             ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
