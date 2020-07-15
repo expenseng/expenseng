38afbf9bfc6dc6ab562a58b5760688b5e024b133
yanmifeakeju <yanmifeakeju@gmail.com>
2020-07-15 04:17:11 +0100

    integrating registration modal

---
+++ public/js/subscription.js
@@ -0,0 +1,20 @@
+const close = document.getElementById("close");
+const open = document.getElementById("open");
+const modal = document.getElementById("modal");
+
+open.addEventListener("click", () => {
+    document.body.classList.toggle("modal-open");
+    modal.classList.add("show-modal");
+});
+
+close.addEventListener("click", () => {
+    document.body.classList.toggle("modal-open");
+    modal.classList.remove("show-modal");
+});
+
+window.addEventListener("click", (e) => {
+    e.target == modal ? modal.classList.remove("show-modal") : false;
+});
+
+// height: 100vh;
+// overflow-y: hidden;
