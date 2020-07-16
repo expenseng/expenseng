6010ab9a4da01bf907d903a9bbd9abe5b563ded4
AironDev <airondev@gmail.com>
2020-07-16 22:12:08 +0100

    Jqury error which prevents admin navbar items from toggling FIXED

--- resources/views/backend/users/index.blade.php
+++ resources/views/backend/users/index.blade.php
@@ -7,9 +7,13 @@
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="/extras/datatables/css/fixedHeader.bootstrap4.css">
     <script  type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+
+    <!-- causes toggle error in navbar -->
+    <!-- <script src="https://code.jquery.com/jquery-3.5.1.js"></script> -->
+
+    <!-- <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script> -->
     <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+    
     <style type="text/css">
         .dataTable>tbody>tr>td, 
         .dataTable>tbody>tr>th, 
