b5fc7c3e531ccce5866cf2d619e30741393742ef
horlathunbhosun <horlathunbhosun@gmail.com>
2020-07-16 19:23:34 +0100

    changes

--- database/migrations/2020_07_13_170758_create_subscriptions_table.php
+++ database/migrations/2020_07_13_170758_create_subscriptions_table.php
@@ -15,6 +15,7 @@ class CreateSubscriptionsTable extends Migration
     {
         Schema::create('subscriptions', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->string('email') ;
             $table->string('subscription_type');
             $table->timestamps();
--- resources/views/pages/expense/index.blade.php
+++ resources/views/pages/expense/index.blade.php
@@ -35,16 +35,18 @@
         <p class="section-heading-paragraph">Expense report gives an insight to how much is being spent by the federal government on a daily basis and how much is spent in major sectors in Nigeria.</p>
         <h5>Subscribe to daily Expense Report</h5>
         <span>
-            <form method="POST" action="{{ url('expense/report')}}">
+            <form method="POST" action="{{ url('subscribe/report')}}">
               @csrf
-              <input type="text" class="form-control" name="email" placeholder="Enter Your Email" style="width: 50%;">
+              <input type="text" class="form-control" name="name" placeholder="Enter Your Name  " style="width: 50%;"><br>
+              <input type="email" class="form-control" name="email" placeholder="Enter Your Email" style="width: 50%;">
+
               <br>
               <select name="subscription_type" id="" class="form-control" style="width: 50%;">
                   <option value="">Select The Report type</option>
                   <option value="ministry">Ministry</option>
                   <option value="contract">Contract</option>
                   <option value="daily&expense">Daily Expense</option>
-                  <option value="payment">Payments W/0 description</option>
+                  <option value="payment">Payments Without description</option>
               </select>
               <button type="submit" class="btn-sm filter">Subscribe</button>
           </form>
