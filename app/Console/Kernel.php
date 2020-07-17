7d5ba51670ce3eddf1df03be850214117100dfb5
Onifs10 <onifadebolu64@gmail.com>
2020-07-17 19:49:58 +0100

    change_schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
          $schedule->command('SendTweet')->monthly();
-         $schedule->command('budgetTweet')->daily();
+         $schedule->command('budgetTweet')->weekly()->mondays()->at('13:00');
     }
 
     /**
