20e0bf3e9fce5e9523117ce6562c682fba7d29d5
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-07-18 08:20:28 +0100

    fix:add loading spinner to avatar, fixed downvote button

--- resources/js/components/Comment/UserImage.vue
+++ resources/js/components/Comment/UserImage.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <div class="lds-ring" v-if="busy"><div></div><div></div><div></div><div></div></div>
         <img :src="this.image" class="resize" :class="isSmall ? 'h-50' : ''" alt="profile-image">
     </div>
 </template>
@@ -19,6 +20,7 @@ export default {
         const image = this.commentService.getAvatar(this.ownerId);
         if(typeof image == "object"){
             image.then(response => {
+                this.busy = false;
                 this.image = response;
             })   
         }else{
@@ -39,3 +41,41 @@ export default {
     }
 }
 </script>
+
+<style lang="css" scoped>
+    .lds-ring {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border: 8px solid #00945e;
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: #00945e transparent transparent transparent;
+}
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
