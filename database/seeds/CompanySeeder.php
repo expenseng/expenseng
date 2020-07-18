759ed69b452b64afcc632ae0d67e7dea8de48d46
David Alenoghena <43188489+davidalenoghena@users.noreply.github.com>
2020-07-18 12:49:24 +0100

    Add files via upload

--- database/seeds/CompanySeeder.php
+++ database/seeds/CompanySeeder.php
@@ -14,48 +14,81 @@ class CompanySeeder extends Seeder
     {
         //
         DB::table('companies')->insert([
-            [
-                "name"=> "Dangote Group",
-                "shortname" => "dangote",
-                "twitter"=> "@dangote_group",
-                "ceo"=> "Aliko Dangote",
-                "industry"=> "@a_dangote"
-            ],
-            [
-                "name"=> "WATERBASE ENGINEERING LIMITED",
-                "shortname" => "waterbase",
-                "twitter"=> "@waterbase_ng",
-                "ceo"=> "Chima Nwogu",
-                "industry"=> "@c_nwogu"
-            ],
-            [
-                "name"=> "AKINYOSOYE  OLADOTUN",
-                "shortname" => "akinsoye",
-                "twitter"=> "@akinsonye_oladotun",
-                "ceo"=> "AKINYOSOYE  OLADOTUN",
-                "industry"=> "@akinsonye_oladotun"
-            ],
-            [
-                "name"=> "MARITIME PROJECT SERVICES LIMITED",
-                "shortname" => "maritime",
-                "twitter"=> "@maritimeprojectservice",
-                "ceo"=> "Moses Babalola",
-                "industry"=> "@m_babalola"
-            ],
-            [
-                "name"=> "GEOID CONSULT LTD",
-                "shortname" => "geoid",
-                "twitter"=> "@geoidconsultltd",
-                "ceo"=> "Aluko Francis",
-                "industry"=> "@a_francis"
-            ],
-            [
-                "name"=> "JAY JAY BADMUS INVESTMENT LTD",
-                "shortname" => "jayjay",
-                "twitter"=> "@jayjay_badmusltd",
-                "ceo"=> "Martin King",
-                "industry"=> "@martinking"
-            ]
+        [
+            "snumber"=> "1",
+            "service" => "Revised Estimated Total Cost 
+            for the Constructions of 
+            2X60MVA, 132/33KV 
+            Substation at Odogunyan, 
+            2X60MVA, 132/33KV 
+            Substation at Ayobo, Ikeja 
+            West-Ayobo 132KV D/C 
+            Transmission Lines and 
+            2X132KV Lines Bays Extension 
+            at Ikeja West",
+            "companyname"=> "LAGA CE Power Limited",
+            "cost"=> "N3,502,347,285.84",
+            "duration"=> "3 Months"
+        ],
+        [
+            "snumber"=> "2",
+            "service" => "Award of contract for Soil 
+            Erosion and Flood Control 
+            Project in Kwoi Town, Jaba
+            LGA, Kaduna State",
+            "companyname"=> "Harris & 
+            Dome Nigeria Limited",
+            "cost"=> "N899,391,526.78",
+            "duration"=> "9 Months"
+        ],
+        [
+            "snumber"=> "3",
+            "service" => "Award of contract for the 
+            Development of IT Integrated 
+            Automation and Interactive 
+            Portal for the Ministry of 
+            Mines and Steel Development",
+            "companyname"=> "Westblue 
+            Nigeria Limited",
+            "cost"=> "N732,966,461.03",
+            "duration"=> "5 Months"
+        ],
+        [
+            "snumber"=> "4",
+            "service" => "Award of contract for the 
+            completion of Sabke/Dutsi and 
+            Mashi Water Supply Project in 
+            Katsina State",
+            "companyname"=> "Bran & 
+            Luebbe Water 
+            Engineers (Nig.) 
+            Limited",
+            "cost"=> "N1,735,272,435.33",
+            "duration"=> "18 months"
+        ],
+        [
+            "snumber"=> "5",
+            "service" => "Award of contract for Direct 
+            Procurement and Acquisition 
+            of Microsoft Office 365 
+            Enterprise License by the 
+            National Pension Commission",
+            "companyname"=> "Microsoft 
+            Ireland",
+            "cost"=> "N164,300,086.92",
+            "duration"=> " "
+        ],
+        [
+            "snumber"=> "6",
+            "service" => "Revised Estimated Total Cost 
+            (RETC) of the contract for the 
+            upgrade and rehabilitation of
+            the Kaduna International Airport Terminal",
+            "companyname"=> "Dari 
+            Investment Limited",
+            "cost"=> "N1,164,523,063.09",
+            "duration"=> "6 Months"
+        ]
         ]);
     }
 }
