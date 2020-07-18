d5b3859f80e64af0f4d4e1a4e22fbed63be3df49
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-07-18 12:14:52 +0100

    fix:change query string accessor

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -89,7 +89,7 @@ class CommentController extends Controller
     public function show(Request $request){
         $response = $this->http->get('comments', [
             'query' => [
-                'origin' => urlencode($request->query('origin')),
+                'origin' => urlencode($request->origin),
             ]
         ]);
 
