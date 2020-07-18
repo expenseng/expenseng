7c80932ab04cd323d396609c7d1768de13da2205
David Alenoghena <43188489+davidalenoghena@users.noreply.github.com>
2020-07-18 14:55:30 +0100

    Add files via upload

--- database/seeds/CompanySeeder.php
+++ database/seeds/CompanySeeder.php
@@ -15,8 +15,8 @@ class CompanySeeder extends Seeder
         //
         DB::table('companies')->insert([
         [
-            "snumber"=> "1",
-            "service" => "Revised Estimated Total Cost 
+            "name" =>"1",
+            "shortname" => "Revised Estimated Total Cost 
             for the Constructions of 
             2X60MVA, 132/33KV 
             Substation at Odogunyan, 
@@ -26,68 +26,68 @@ class CompanySeeder extends Seeder
             Transmission Lines and 
             2X132KV Lines Bays Extension 
             at Ikeja West",
-            "companyname"=> "LAGA CE Power Limited",
-            "cost"=> "N3,502,347,285.84",
-            "duration"=> "3 Months"
+            "industry"=> "LAGA CE Power Limited",
+            "ceo"=> "N3,502,347,285.84",
+            "twitter"=> "3 Months"
         ],
         [
-            "snumber"=> "2",
-            "service" => "Award of contract for Soil 
+            "name" =>"2",
+            "shortname" => "Award of contract for Soil 
             Erosion and Flood Control 
             Project in Kwoi Town, Jaba
             LGA, Kaduna State",
-            "companyname"=> "Harris & 
+            "industry"=> "Harris & 
             Dome Nigeria Limited",
-            "cost"=> "N899,391,526.78",
-            "duration"=> "9 Months"
+            "ceo"=> "N899,391,526.78",
+            "twitter"=> "9 Months"
         ],
         [
-            "snumber"=> "3",
-            "service" => "Award of contract for the 
+            "name" =>"3",
+            "shortname" => "Award of contract for the 
             Development of IT Integrated 
             Automation and Interactive 
             Portal for the Ministry of 
             Mines and Steel Development",
-            "companyname"=> "Westblue 
+            "industry"=> "Westblue 
             Nigeria Limited",
-            "cost"=> "N732,966,461.03",
-            "duration"=> "5 Months"
+            "ceo"=> "N732,966,461.03",
+            "twitter"=> "5 Months"
         ],
         [
-            "snumber"=> "4",
-            "service" => "Award of contract for the 
+            "name" =>"4",
+            "shortname" => "Award of contract for the 
             completion of Sabke/Dutsi and 
             Mashi Water Supply Project in 
             Katsina State",
-            "companyname"=> "Bran & 
+            "industry"=> "Bran & 
             Luebbe Water 
             Engineers (Nig.) 
             Limited",
-            "cost"=> "N1,735,272,435.33",
-            "duration"=> "18 months"
+            "ceo"=> "N1,735,272,435.33",
+            "twitter"=> "18 months"
         ],
         [
-            "snumber"=> "5",
-            "service" => "Award of contract for Direct 
+            "name" =>"5",
+            "shortname" => "Award of contract for Direct 
             Procurement and Acquisition 
             of Microsoft Office 365 
             Enterprise License by the 
             National Pension Commission",
-            "companyname"=> "Microsoft 
+            "industry"=> "Microsoft 
             Ireland",
-            "cost"=> "N164,300,086.92",
-            "duration"=> " "
+            "ceo"=> "N164,300,086.92",
+            "twitter"=> " "
         ],
         [
-            "snumber"=> "6",
-            "service" => "Revised Estimated Total Cost 
+            "name" =>"6",
+            "shortname" => "Revised Estimated Total Cost 
             (RETC) of the contract for the 
             upgrade and rehabilitation of
             the Kaduna International Airport Terminal",
-            "companyname"=> "Dari 
+            "industry"=> "Dari 
             Investment Limited",
-            "cost"=> "N1,164,523,063.09",
-            "duration"=> "6 Months"
+            "ceo"=> "N1,164,523,063.09",
+            "twitter"=> "6 Months"
         ]
         ]);
     }
