d5989f0dcf9e1c3edce2435a215ca7cf7b153baf
Onifs10 <onifadebolu64@gmail.com>
2020-07-19 12:28:04 +0100

    ministry_page_fixed

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -532,7 +532,7 @@
                         data:{'tweet': $tweet},
                         success:function (data) {
                             jQuery('.tweet').val('');
-                            jQuery('.alert1').removeClass('alert-danger')
+                            jQuery('.alert1').removeClass('alert-danger');
                             jQuery('.alert1').fadeIn(5000);
                             jQuery('.alert1').addClass('alert alert-success text-white');
                             jQuery('.alert1').html('tweet sent ');
@@ -540,6 +540,7 @@
                             // $('#savingsStatus').html(data);
                     },error: function (data){
                             jQuery('.tweet').val('');
+                            jQuery('.alert1').removeClass('alert-success');
                             jQuery('.alert1').fadeIn(5000);
                             jQuery('.alert1').addClass('alert alert-danger text-white');
                             jQuery('.alert1').html('tweet not sent');
