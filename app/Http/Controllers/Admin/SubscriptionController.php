afa3b9c3c2ba0953c9e4db0fb6b58e4e7e06d30d
horlathunbhosun <horlathunbhosun@gmail.com>
2020-07-19 10:38:52 +0100

    route for subscription paginate

--- app/Http/Controllers/Admin/SubscriptionController.php
+++ app/Http/Controllers/Admin/SubscriptionController.php
@@ -18,8 +18,8 @@ class SubscriptionController extends Controller
             return redirect(route('profile'));
         }
 
-        $subscribe = Subscription::all()->paginate(10);
-        return view('backend.subscription.index')->with(['subscribe' => $subscribe]);
+        $subscribe = Subscription::all()->simplePaginate(10);
+        return view('backend.subscription.index', compac('subscribe'));
     }
 
 
