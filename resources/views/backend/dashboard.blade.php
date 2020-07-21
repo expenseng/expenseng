7dbafb89f98d1d851cd6d458eb164a72233c6379
Onifs10 <onifadebolu64@gmail.com>
2020-07-21 11:42:46 +0100

    TweetDeleteFeatureAdded

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -206,7 +206,7 @@
         <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Type in tweet below</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Tweets</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">×</span>
                 </button>
