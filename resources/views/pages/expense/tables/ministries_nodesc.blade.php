3c95bef7a5f4f2d952fd2e96b6bdaf69a841e9cd
papa28x4 <swissfair@yahoo.co.uk>
2020-07-22 18:58:31 +0100

    expense/ministry filter implementation

---
+++ resources/views/pages/expense/tables/ministries_nodesc.blade.php
@@ -0,0 +1,30 @@
+<div style="overflow-x: auto;">
+    <table cell-spacing="0" data-pagination="true" data-page-size="10" class="table table-striped table-responsive-sm">
+        <thead>
+            <tr>
+                <th class="section-shadow row-ministry">Ministry</th>
+                <th class="row-company">Company</th>
+                <th class="row-amount">Amount</th>
+                <th class="row-date">Date</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($collection['nondescriptive'] as $expense)
+                <tr>
+                    <td class="section-shadow">
+                        <a href="{{ route('ministries.single', ['ministry' => strtolower($expense->ministry()['shortname']) ]) }}" class="text-success">
+                            {{ucfirst($expense->ministry()['name'])}}
+                        </a>
+                    </td>
+                    <td>{{$expense->beneficiary}}</td>
+                    <td>&#8358;{{$expense->amount()}}</td>
+                    <td>{{ date('d-m-Y', strtotime($expense->payment_date))}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+<div class="table-footer d-flex align-items-center pagination1">
+    <span>{{ $collection['nondescriptive']->firstItem() }} - {{ $collection['nondescriptive']->lastItem() }} of {{ $collection['nondescriptive']->total() }} results</span>
+    {{ $collection['nondescriptive']->links() }}
+</div>
