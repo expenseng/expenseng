3a550123cc3e5b299c9ece663e74f57bcfd131a3
Lilian Ovrawah <lilianovrawah@gmail.com>
2020-07-24 11:33:23 +0100

    ministry and contractor page breadcrumb

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'ExpenseNG') }}</title>
+    <title>{{ config('app.name', 'ExpenseNG') }}</title>  
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
