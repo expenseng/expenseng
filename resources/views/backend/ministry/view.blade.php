b4fa8def29a1188cf6eec104d0b845e0dde6f9b3
Ezeko <ezeko2017@gmail.com>
2020-07-29 19:53:00 +0100

    bug
    make datatable work on firefox for cabinet

--- resources/views/backend/ministry/view.blade.php
+++ resources/views/backend/ministry/view.blade.php
@@ -2,10 +2,9 @@
 @push('css')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" type="text/css" href="/extras/datatables/css/buttons.bootstrap4.css">
+    
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="/extras/datatables/css/fixedHeader.bootstrap4.css">
-    <script src="/extras/jquery/jquery-3.3.1.min.js"></script>
+    
     <script  type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
     <link rel="stylesheet" href="{{ asset('css/dash-table.css') }}">
     <!-- causes toggle error in navbar -->
