92f05d9637d5fc0064185a2d7cbe5bd240d9af89
papa28x4 <swissfair@yahoo.co.uk>
2020-08-03 06:40:33 +0100

    clean up

--- public/js/index.js
+++ public/js/index.js
@@ -182,17 +182,6 @@ $(".comp-card").click(function() {
     return false;
 });
 
-// $(".ministry-stat").click(function() {
-//     let ministry_list = document.getElementById('ministry_list');
-//     let ministry = ministry_list.value;
-
-//     let ministry_card = document.getElementsByClassName('ministry-stat');
-//     let link = document.getElementById('link');
-//     link.href = `/ministries/${ministry}`;
-//     window.location = $(this).find("a").attr("href"); 
-//     return false;
-// });
-
 $(".ministry-stat").click(function(e) {
     console.log(e.target)
     let link = document.getElementById('link');
