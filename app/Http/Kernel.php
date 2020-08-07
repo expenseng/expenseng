a95263f5b2c34c4d8df8b4977e80c3c3a6698c67
AironDev <airondev@gmail.com>
2020-08-07 18:55:39 +0100

    added Monitoring and Inspection service

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,11 +37,13 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Inspector\Laravel\Middleware\WebRequestMonitoring::class,
         ],
 
         'api' => [
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Inspector\Laravel\Middleware\WebRequestMonitoring::class,
         ],
     ];
 
