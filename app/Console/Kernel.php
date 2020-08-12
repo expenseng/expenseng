d7672dd68dffb6b25e2d079d2855c369df101f5f
Onifs10 <onifadebolu64@gmail.com>
2020-08-12 12:18:52 +0100

    tweetFixed

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,18 +27,24 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('SendTweet daily')->everyThirtyMinutes()
-             ->between('12:00', '13:59');
-         $schedule->command('SendTweet daily')->everyThirtyMinutes()->between('9:00', '10:40');
-         $schedule->command('SendTweet past')->everyThirtyMinutes()->weekends()->between('2:00', '6:00');
+         $schedule->command('SendTweet daily')
+             ->hourly()
+             ->timezone('Africa/Lagos')
+             ->between('08:01', '11:30');
+         $schedule->command('SendTweet past')
+             ->hourly()
+             ->weekends()
+             ->timezone('Africa/Lagos')
+             ->between('2:00', '6:00');
 //         $schedule->command('budgetTweet')->weekly()->mondays()->at('13:00');
          $schedule->command('ReportLogging')->daily()->at('02:00');
          $schedule->command('parse:sheet')
              ->daily();
-         $schedule->command('download:reports')->everyTwoMinutes();
+//         $schedule->command('download:reports')->everyTwoMinutes();
          $schedule->command('queue:work --once --queue=ceoSearch')->everyFourHours();
          $schedule->command('queue:work  --queue=default --stop-when-empty')->daily();
     }
+
 
     /**
      * Register the commands for the application.
