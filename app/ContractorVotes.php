cf81f02cc541143be358a15d7062060266993a14
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-09-23 15:05:08 +0100

    add contractor votes model

---
+++ app/ContractorVotes.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ContractorVotes extends Model
+{
+    protected $table = "contractor_votes";
+    protected $guarded = [];
+}
