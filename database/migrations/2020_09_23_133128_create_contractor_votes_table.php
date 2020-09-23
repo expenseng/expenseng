90980d54c6a034040ea30745fd050c0527d3bfa7
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-09-23 14:38:51 +0100

    add contractor_votes table

---
+++ database/migrations/2020_09_23_133128_create_contractor_votes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContractorVotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contractor_votes', function (Blueprint $table) {
+            $table->id();
+            //here we can insert the names of 
+            //the contractor types which may increase in future
+            $table->string('type');
+            $table->integer('count');
+            $table->integer('contractor_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contractor_votes');
+    }
+}
