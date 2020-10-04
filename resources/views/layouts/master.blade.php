ea4323b9db27908ec5b3692e6e195ec04799df3e
papa28x4 <swissfair@yahoo.co.uk>
2020-10-04 18:49:29 +0100

    jquery issues

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,8 +14,8 @@
       @include('partials.footer')
     </div>
   </body> 
-    @yield('js')
     <script src="{{ asset('js/app.js') }}" type="application/javascript"></script>
+    @yield('js')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tooltipster/3.3.0/js/jquery.tooltipster.min.js"></script>
     <script src="{{ asset('js/index.js') }}"></script>
 </html>
