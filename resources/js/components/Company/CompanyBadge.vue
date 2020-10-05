50035d707881f3fa197c05021af113e33b6c2b99
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-10-05 09:53:59 +0100

    feat:display contractor type badge correctyl

--- resources/js/components/Company/CompanyBadge.vue
+++ resources/js/components/Company/CompanyBadge.vue
@@ -29,7 +29,7 @@ export default {
                 }
             })
             .catch(err => {
-                console.log(err);
+                console.log("error-badge" + err);
             })
     },
 }
